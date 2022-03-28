fa7e2f313fe3db6bc0f8552997547b5ef33a9a2a
NamNguyen-hub <nam400@gmail.com>
2022-03-28 16:30:34 -0500

    220328

--- Codes/00. Test codes.R
+++ Codes/00. Test codes.R
@@ -1,3 +1,55 @@
 ## Stata installation
 
-install.packages('RStata')
+#install.packages('RStata')
+
+library('RStata')
+
+options("RStata.StataPath" = "\"C:\\Program Files\\Stata16\\StataMP-64\"")
+options("RStata.StataVersion" = 16)
+
+stata("help regress")
+
+chooseStataBin()
+options("RStata.StataPath")
+
+stata_path = "\"C:\\Program Files\\Stata16\\StataMP-64\""
+
+stata(stata.path=stata_path,
+      c("set obs 200", "gen a = 1"))
+
+stata_src = "
+version 10
+sysuse auto
+reg mpg weight
+"
+stata(stata.path=stata_path,
+      stata_src)
+
+
+## Data input stata
+x <- data.frame(a = rnorm(3), b = letters[1:3])
+stata("sum a", data.in = x)
+
+library('RStata')
+
+#probit crisis gap_h1, vce(cluster id)
+
+stata_src = "
+probit crisis credit_gap, vce(cluster id)
+predict crisis_hat
+lroc
+graph export mygraph1.pdf, replace
+twoway scatter crisis_hat crisis credit_gap, connect(l i) msymbol(i O) sort ylabel(0 1)
+graph export mygraph2.pdf, replace
+"
+
+stata(stata_src, data.in=dfh2)
+
+
+## Data output stata 
+auto <- stata("sysuse auto", data.out = TRUE)
+
+# Test 1 with crisis data
+stata("sum crisis", data.in=df)
+
+
--- Codes/02. Process Data.R
+++ Codes/02. Process Data.R
@@ -7,6 +7,7 @@ library(reshape2)
 library(dplyr)
 library(rstudioapi)
 setwd(dirname(getActiveDocumentContext()$path))
+#library(time)
 
 startdate="2000-01-01"
 enddate="2017-10-01"
@@ -17,11 +18,12 @@ df1$date<-as.Date(df1$date)
 min(as.Date((df1$date)))
 names(df1)    
 
-df2 <- read.table("../data/Input/crisis.csv", header=TRUE, sep=",")
+df2 <- read.table("../data/Input/crisis_h4.csv", header=TRUE, sep=",")
     # df3 <- read.table("HHCredit_GDP_gap.txt", header=TRUE, sep=",")
     # df4 <- read.table("NFECredit_GDP_gap.txt", header=TRUE, sep=",")
     # df5 <- read.table("GDP_gap.txt", header=TRUE, sep=",")
     # df6 <- read.table("HP_inc_gap.txt", header=TRUE, sep=",")
+df2$date <- as.Date(df2$date)
 names2 <- names(df2)
 names2
 ## Limit df1 year and countries here
@@ -37,10 +39,63 @@ names(df1)<- c("date","id","credit_gap")
 ## Limit df2 year and countries here
 df2 = subset(df2, date >= as.Date(startdate)) 
 df2 <- melt(df2, id=c("date"))
+
+
 names(df2)<- c("date","id","crisis")
+
+
 
 # Merge the two df by ID and date
 df <- merge(df1, df2, by=c("id","date"), all=TRUE)
+#df$date <- as.Date(df$date)
+
+dfh2<- df %>%
+  group_by(id) %>%
+  #mutate(gap= dplyr::lag(credit_gap, n=2)) #%>%
+  na.omit()
+
+  
+  
+  
+dfh8<- df %>%
+  group_by(id) %>%
+  mutate(gap= dplyr::lag(credit_gap, n=8)) #%>%
+  #na.omit()
+
+
+
+library(dplyr)
+df<- df %>%
+  group_by(id) %>%
+  mutate(gap_h1= dplyr::lag(credit_gap, n=1)) %>%
+  mutate(gap_h2= dplyr::lag(credit_gap, n=2)) %>%
+  mutate(gap_h3= dplyr::lag(credit_gap, n=3)) %>%
+  mutate(gap_h4= dplyr::lag(credit_gap, n=4)) %>%
+  mutate(gap_h5= dplyr::lag(credit_gap, n=5)) %>%
+  mutate(gap_h6= dplyr::lag(credit_gap, n=6)) %>%
+  mutate(gap_h7= dplyr::lag(credit_gap, n=7)) %>%
+  mutate(gap_h8= dplyr::lag(credit_gap, n=8)) %>%
+  mutate(gap_h9= dplyr::lag(credit_gap, n=9)) %>%
+  mutate(gap_h10= dplyr::lag(credit_gap, n=10)) %>%
+  mutate(gap_h11= dplyr::lag(credit_gap, n=11)) %>%
+  mutate(gap_h12= dplyr::lag(credit_gap, n=12))
+
+
+
+df <- df %>% group_by(id) %>% mutate(gap_h1 = stats::tlag(credit_gap, order_by =date, k =1))
+       ## https://stackoverflow.com/questions/8910268/general-lag-in-time-series-panel-data                          
+df<-df[with(df, order(id,date)), ]
+
+library(plm)
+df<-pdata.frame(df,index=c("id","date"))  
+df<-transform(df, gaph1=plm::lag(credit_gap,k=1))   
+
+
+library(xts)
+library(tsbox)
+df <- xts(df,order.by=as.Date(df$date))
+
+credit_gap_h1 <- ts_lag(df$credit_gap, by =1)
 
 # pglm Panel probit regression
 
@@ -71,3 +126,9 @@ summary(model1)
     # df1 <- df1[order(df1$ID, df1$date),]
     # 
     # df1<-na.omit(df1)
+
+
+
+# Test 2
+library(collapse)
+L(df, n=1, by = 'id', t='date', cols=4)
